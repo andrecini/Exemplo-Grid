b40505ee166aa1bcf54016f8d48ee020109b7362
andrecini <andrevpcini@gmail.com>
2020-10-28 22:46:03 -0300

    Exemplo

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace EX_GridForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Grid1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Grid1
+            // 
+            this.Grid1.AllowUserToDeleteRows = false;
+            this.Grid1.AllowUserToResizeColumns = false;
+            this.Grid1.AllowUserToResizeRows = false;
+            this.Grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid1.Location = new System.Drawing.Point(12, 123);
+            this.Grid1.Name = "Grid1";
+            this.Grid1.ReadOnly = true;
+            this.Grid1.Size = new System.Drawing.Size(743, 374);
+            this.Grid1.TabIndex = 0;
+            this.Grid1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Grid1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(571, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SAC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 522);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Grid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Grid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Grid1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX_GridForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        DataTable Tbl;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            // Cria uma nova tabela
+            Tbl = new DataTable();
+            // Adiciona os nomes e tipos das colunas
+            Tbl.Columns.Add("Meses", typeof(int));
+            Tbl.Columns.Add("Saldo Devedor (R$)", typeof(double));
+            Tbl.Columns.Add("Amortização", typeof(double));
+            Tbl.Columns.Add("Juros", typeof(double));
+            Tbl.Columns.Add("Prestação", typeof(double));
+            Tbl.Columns.Add("Prestação acumulada", typeof(double));
+            Tbl.Columns.Add("Juros Acumulado", typeof(double));
+            
+            // Declara classe para gerenciar registros
+            DataRow Linha;
+            // ******************** Forma 1 de criar registro
+            // e acrescentar no DataGrid
+
+
+            int prestacoes = 360;
+            double taxa = 0.01;
+            double[] SaldoDevedor = new double[prestacoes + 1];
+            double amortizacao;
+            double[] juros = new double[prestacoes + 1];
+            double[] parcelas = new double[prestacoes + 1];
+            double jAcumulado = 0;
+            double pAcumulado = 0;
+           
+            SaldoDevedor[0] = 300000;
+            juros[0] = 0;
+
+            /// Calculo da Amortização:
+            amortizacao = SaldoDevedor[0] / 360;
+
+            for (int i = 1; i < juros.Length; i++)
+            {
+                /// Calculo do Saldo Devedor:
+                SaldoDevedor[i] = SaldoDevedor[i - 1] - amortizacao;
+                /// Cálculo do juros:
+                juros[i] = SaldoDevedor[i] * taxa;
+
+                /// Calculo do Valor da Parcela:
+                parcelas[i] = juros[i] + amortizacao;                
+
+                /// Calculo do Juros Acumulado:
+                jAcumulado += juros[i];
+
+                /// Calculo das parcelas acumuladas:
+                pAcumulado += parcelas[i];
+
+                amortizacao = SaldoDevedor[0] / 360;
+
+                /// Aredondamento:
+                amortizacao = Math.Round(amortizacao, 2);
+                juros[i] = Math.Round(juros[i], 2);
+                parcelas[i] = Math.Round(parcelas[i], 2);
+                SaldoDevedor[i] = Math.Round(SaldoDevedor[i], 2);
+                jAcumulado = Math.Round(jAcumulado, 2);
+                pAcumulado = Math.Round(pAcumulado, 2);
+
+                /// Cria uma nova linha
+                Linha = Tbl.NewRow();
+                /// Coloca os dados em cada uma das colunas desta linha
+                Linha[0] = i;
+                Linha[1] = SaldoDevedor[i];
+                Linha[2] = amortizacao;
+                Linha[3] = juros[i];
+                Linha[4] = parcelas[i];
+                Linha[5] = jAcumulado;
+                Linha[6] = pAcumulado;
+
+                /// Adiciona a linha na tabela
+                Tbl.Rows.Add(Linha);
+            }
+            Grid1.DataSource = Tbl;
+        }
+    }
+}
